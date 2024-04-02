5b8fdfd7ae4c1bd0d80ac847c7fafd636eea5f1f
odyg <100243742+odyg@users.noreply.github.com>
2024-04-02 07:45:24 -0600

    Add project files.

---
+++ Models/CarModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalApp.Models // Change to your actual namespace
+{
+    public class CarModel
+    {
+        [Key]
+        public int CarId { get; set; }
+
+        [Required, StringLength(100)]
+        public string Make { get; set; }
+
+        [Required, StringLength(100)]
+        public string Model { get; set; }
+
+        [Required, StringLength(20)]
+        public string Type { get; set; }
+
+        [Range(1450, 2050)]
+        public int Capacity { get; set; }
+        
+        [Range(1450, 2050)]
+        public int Year
+        {
+            get; set;
+        }
+
+        [Required, StringLength(20)]
+        public string LicensePlate { get; set; }
+
+        [Required, Range(0, 1000)]
+        public double DailyRate
+        {
+            get; set;
+        }
+
+        [Required, StringLength(50)]
+        public string IsAvailable
+        {
+            get; set;
+        }
+    }
+}
---
+++ Models/ReserveModel.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CarRentalApp.Models
+{
+    public class ReserveModel
+    {
+        [Key]
+        public int RentId
+        {
+            get; set;
+        }
+
+        [Required]
+        public int CarId
+        {
+            get; set;
+        } // Foreign key from Book model
+
+        [Required]
+        public int RenterId
+        {
+            get; set;
+        } // Foreign key from Reader model
+
+        [DataType(DataType.Date)]
+        public DateTime BorrowDate
+        {
+            get; set;
+        }
+
+        [DataType(DataType.Date)]
+        public DateTime ReserveDate
+        {
+            get; set;
+        }
+
+        [DataType(DataType.Date)]
+        public DateTime? ReturnDate
+        {
+            get; set;
+        } // Nullable for cases when the book hasn't been returned yet
+        
+        [Required]
+        public string Status
+        {
+            get; set;
+        }
+
+        [Required]
+        public double TaxRate
+        {
+            get; set;
+        }
+
+        [Required]
+        public double TotalAmount
+        {
+            get; set;
+        }
+
+        // Navigation properties
+        public CarModel Car
+        {
+            get; set;
+        }
+        public RenterModel Renter
+        {
+            get; set;
+        }
+    }
+}
+
