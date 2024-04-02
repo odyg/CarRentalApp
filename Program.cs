2c5f1c5792a092a9d56237944d3394b0ed2aa7a6
odyg <100243742+odyg@users.noreply.github.com>
2024-04-02 07:54:18 -0600

    intital

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+//using Assignment4.Data;
+//using Assignment4.Repositories;
+//using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<LMSDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultString")));
+
+//builder.Services.AddScoped<BookRepository>();
+//builder.Services.AddScoped<ReaderRepository>();
+//builder.Services.AddScoped<BorrowingRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapControllers();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
